e78a7acafc58cc3825b7de668f9ee40b2f4c0f4d
Clemens Auinger <clemensauinger@gmail.com>
2025-12-17 18:43:56 +0100

    changed OnConfiguring

--- backend/WebApplication/MyWebApplication.Db/WeatherDatabaseContext.cs
+++ backend/WebApplication/MyWebApplication.Db/WeatherDatabaseContext.cs
@@ -17,8 +17,6 @@ public class WeatherDatabaseContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        //base.OnConfiguring(optionsBuilder);
-
-        //optionsBuilder.UseSqlServer("Server=tcp:sql-server-auingerc.database.windows.net,1433;Initial Catalog=sql-database-auingerc;Persist Security Info=False;User ID=auingerc;Password=htlgkr1!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        base.OnConfiguring(optionsBuilder);
     }
 }
